44e8bd74a77eb37b6fede447dbe3c1ef6cb4a9b4
Jaime Espinueva <espinuevajaime@icloud.com>
2017-12-20 08:41:24 +0800

    make test build

---
+++ tests/Feature/SystemNotificationTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Handymaster\SystemNotification\Tests\Feature;
+
+use Handymaster\SystemNotification\Tests\TestCase;
+
+class SystemNotificationTest extends TestCase
+{
+    /** @test */
+    public function notification_info_test()
+    {
+        $this->assertTrue(true);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,43 @@
 
 namespace Handymaster\SystemNotification\Tests;
 
+use Handymaster\SystemNotification\SystemNotificationServiceProvider;
+use Handymaster\SystemNotification\Facade\SystemNotification;
+
+
 abstract class TestCase extends \Orchestra\TestBench\TestCase
 {
+    /**
+     * Setup test
+     */
+    protected function setUp($app): void
+    {
+        parent::setUp();
 
+
+
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app->make('Illuminate\Contracts\Http\Kernel')->pushMiddleware('Illuminate\Session\Middleware\StartSession');
+    }
+
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+          SystemNotificationServiceProvider::class
+        ];
+    }
+
+    protected function getApplicationAliases($app)
+    {
+        return [
+            'SystemNotification' => SystemNotification::class
+        ];
+    }
 }
